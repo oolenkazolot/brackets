f82d4384207928f934b41334199656bb353e21b6
oolenkazolot <oolenka.zolot@gmail.com>
2022-07-14 17:23:16 +0200

    feat: create brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = str.split('');
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+        arr.splice(i, 2);
+        i = -1;
+        break;
+      }
+    }
+  }
+  if (arr.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
